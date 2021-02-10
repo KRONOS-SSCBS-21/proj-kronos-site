833b243b4ea4cb72495fc0eead90d7b6a60f90fc
paprajapati9 <paprajapati9@gmail.com>
2021-02-10 15:49:27 +0530

    updated header and added 1st cover section

---
+++ src/js/header.js
@@ -0,0 +1,22 @@
+window.addEventListener("DOMContentLoaded", () => {
+
+    /**
+     * Create sticky header that hides on scoll down and show on scroll up
+     */
+
+    $('body').css('padding-top', $('.navbar').outerHeight() + 'px')
+    // detect scroll top or down
+    if ($('.smart-scroll').length > 0) { // check if element exists
+        var last_scroll_top = 0;
+        $(window).on('scroll', function() {
+            scroll_top = $(this).scrollTop();
+            if(scroll_top < last_scroll_top) {
+                $('.smart-scroll').removeClass('scrolled-down').addClass('scrolled-up');
+            }
+            else {
+                $('.smart-scroll').removeClass('scrolled-up').addClass('scrolled-down');
+            }
+            last_scroll_top = scroll_top;
+        });
+    }
+})
