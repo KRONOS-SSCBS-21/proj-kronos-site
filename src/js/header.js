5cb92706bcdce02c450e44638a85dc771185669d
paprajapati9 <paprajapati9@gmail.com>
2021-02-10 16:57:06 +0530

    modified header and 1st block

--- src/js/header.js
+++ src/js/header.js
@@ -4,13 +4,17 @@ window.addEventListener("DOMContentLoaded", () => {
      * Create sticky header that hides on scoll down and show on scroll up
      */
 
-    $('body').css('padding-top', $('.navbar').outerHeight() + 'px')
+    $('body .kronos-block-container').css('padding-top', $('.navbar').outerHeight() + 60 + 'px')
     // detect scroll top or down
     if ($('.smart-scroll').length > 0) { // check if element exists
         var last_scroll_top = 0;
         $(window).on('scroll', function() {
             scroll_top = $(this).scrollTop();
-            if(scroll_top < last_scroll_top) {
+            console.log(scroll_top, "scroll top");
+            if(scroll_top == 0) {
+                $('.smart-scroll').removeClass('scrolled-up')
+            }
+            else if(scroll_top < last_scroll_top) {
                 $('.smart-scroll').removeClass('scrolled-down').addClass('scrolled-up');
             }
             else {
