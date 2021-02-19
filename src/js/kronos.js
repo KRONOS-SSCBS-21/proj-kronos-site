39ad7ae949a66ebba115def4fede445b68057618
Ayush Gupta <76724104+ayushgupta0110@users.noreply.github.com>
2021-02-19 13:13:25 +0530

    Added Animated BG and padding in cards

---
+++ src/js/kronos.js
@@ -0,0 +1,16 @@
+// Animated Banner Section
+
+VANTA.WAVES({
+    el: ".kronos-top-block-container",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0xf4e95,
+    shininess: 32.00,
+    waveHeight: 12.00,
+    waveSpeed: 0.50
+  });
