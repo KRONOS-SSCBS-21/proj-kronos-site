7ea1c38b03efcb289c702e4a8f6945b7f383261c
paprajapati9 <paprajapati9@gmail.com>
2021-02-24 10:05:49 +0530

    added modal and multiplr fixes

--- src/js/header.js
+++ src/js/header.js
@@ -13,8 +13,11 @@ window.addEventListener("DOMContentLoaded", () => {
             if(scroll_top == 0) {
                 $('.smart-scroll').removeClass('scrolled-up')
             }
-            else{
-                $('.smart-scroll').addClass('scrolled-up');
+            else if(scroll_top < last_scroll_top) {
+                $('.smart-scroll').removeClass('scrolled-down').addClass('scrolled-up');
+            }
+            else {
+                $('.smart-scroll').removeClass('scrolled-up').addClass('scrolled-down');
             }
             last_scroll_top = scroll_top;
         });
--- src/js/kronos.js
+++ src/js/kronos.js
@@ -1,16 +1,76 @@
-// Animated Banner Section
+window.addEventListener("DOMContentLoaded", () => {
 
-VANTA.WAVES({
-    el: ".kronos-top-block-container",
-    mouseControls: true,
-    touchControls: true,
-    gyroControls: false,
-    minHeight: 200.00,
-    minWidth: 200.00,
-    scale: 1.00,
-    scaleMobile: 1.00,
-    color: 0xf4e95,
-    shininess: 32.00,
-    waveHeight: 12.00,
-    waveSpeed: 0.50
-  });
+	// Animated Banner Section
+	VANTA.WAVES({
+		el: ".kronos-top-block-container",
+		mouseControls: true,
+		touchControls: true,
+		gyroControls: false,
+		minHeight: 200.00,
+		minWidth: 200.00,
+		scale: 1.00,
+		scaleMobile: 1.00,
+		color: 0xf4e95,
+		shininess: 32.00,
+		waveHeight: 12.00,
+		waveSpeed: 0.50
+	});
+
+	/**
+	 * Modal open part
+	 */
+	let buttons = document.querySelectorAll("button[data-toggle='modal']");
+	attachModal(buttons);
+
+	function attachModal(buttons) {
+		if(buttons.length > 0){
+			buttons.forEach(element => {
+				element.addEventListener("click", function (e) {
+					let modalID = element.getAttribute("data-target");
+					let modal = document.getElementById(modalID);
+					if(modal){
+						addModalContent(modal, element.parentElement);
+						modal.style.display = "grid";
+						setTimeout(() => {
+							modal.classList.add("show");
+						}, 100);
+						document.body.classList.add("modal-open");
+					}
+				})
+			});
+		}
+	}
+
+	window.addEventListener("click", function(e) {
+		let modal = document.querySelector(".modal.show");
+		if(modal){
+			let closeButton = modal.querySelector("button.close"),
+				modalContent = modal.querySelector(".modal-content");
+
+			closeButton.addEventListener("click", function(e) {
+				closeModal(modal);
+			});
+			if(e.target != modalContent && !modalContent.contains(e.target)){
+				closeModal(modal);
+			}
+		}
+	})
+
+	function closeModal(modal) {
+		modal.classList.remove("show");
+		setTimeout(() => {
+			modal.style.display = "none";
+		}, 100);
+		document.body.classList.remove("modal-open");
+	}
+
+	function addModalContent(modal, card) {
+		let title = card.querySelector(".card-title"),
+			desc = card.querySelector(".card-text"),
+			modalTitle = modal.querySelector(".modal-title"),
+			modalDesc = modal.querySelector(".modal-body");
+
+		modalTitle.innerHTML = title.innerHTML;
+		modalDesc.innerHTML = desc.innerHTML;
+	}
+})
