97b67e2258ad50f59ee0e838a07679a1fc7acf08
Ayush Gupta <76724104+ayushgupta0110@users.noreply.github.com>
2021-05-13 17:11:08 +0530

    Removed Vantajs code from header.js

--- src/js/kronos.js
+++ src/js/kronos.js
@@ -1,21 +1,5 @@
 window.addEventListener("DOMContentLoaded", () => {
 
-	// Animated Banner Section
-	VANTA.WAVES({
-		el: "#",
-		mouseControls: true,
-		touchControls: true,
-		gyroControls: false,
-		minHeight: 200.00,
-		minWidth: 200.00,
-		scale: 1.00,
-		scaleMobile: 1.00,
-		color: 0x4fc6,
-		shininess: 32.00,
-		waveHeight: 12.00,
-		waveSpeed: 0.50
-	});
-
 	/**
 	 * Modal open part
 	 */
