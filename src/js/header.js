6a100cb4d5c920d53e15a71b9b20aed13216eb11
Pankaj Prajapati <paprajapati9@gmail.com>
2021-11-13 10:30:52 +0530

    multiple ui fixes

--- src/js/header.js
+++ src/js/header.js
@@ -3,10 +3,6 @@ window.addEventListener("DOMContentLoaded", () => {
    * Create sticky header that hides on scoll down and show on scroll up
    */
 
-  $("body .kronos-block-container").css(
-    "padding-top",
-    $(".navbar").outerHeight() + 60 + "px"
-  );
   // detect scroll top or down
   if ($(".smart-scroll").length > 0) {
     // check if element exists
